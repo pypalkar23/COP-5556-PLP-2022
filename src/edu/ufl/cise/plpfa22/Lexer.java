2c025a390a8adc0742363f1fb7dd97aeaa74702a
pypalkar23 <mandar.palkar94@gmail.com>
2022-09-13 12:51:48 -0400

    Initial Commit

---
+++ src/edu/ufl/cise/plpfa22/Lexer.java
@@ -0,0 +1,16 @@
+package edu.ufl.cise.plpfa22;
+
+public class Lexer implements  ILexer {
+    public  Lexer(String input){
+
+    }
+    @Override
+    public IToken next() throws LexicalException {
+        return null;
+    }
+
+    @Override
+    public IToken peek() throws LexicalException {
+        return null;
+    }
+}
